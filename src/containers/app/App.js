324e81b61f97ca31b1711fef852b14fbfcab6db5
杜娟 <jdu.xian@gmail.com>
2019-08-12 13:57:36 +0800

    add test for app

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -1,10 +1,14 @@
-import React, { Component } from "react";
-import "./App.less";
+import React, { Component } from 'react';
+import './App.less';
 
 class App extends Component {
-	render() {
-		return <div className="App">Hello World</div>;
-	}
+  render() {
+    return (
+      <div className="App" data-testid="app">
+        Hello World
+      </div>
+    );
+  }
 }
 
 export default App;
